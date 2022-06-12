f4db5fa1614ec473e5032e2eec69898e31440e9b
kanghyki <kanghyki@gmail.com>
2022-06-12 21:48:59 +0900

    Test: ft_print_memory

--- src/main.c
+++ src/main.c
@@ -6,43 +6,42 @@
 /*   By: kanghyki <kanghyki@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 22:42:17 by kanghyki          #+#    #+#             */
-/*   Updated: 2022/06/12 15:57:01 by kanghyki         ###   ########.fr       */
+/*   Updated: 2022/06/12 21:47:50 by kanghyki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 #include "excuse_cmd/cmd.h"
 
-void	print_memory_test(char *dst, int size)
+void	ft_print_memory(void *dst, int size)
 {
+	const char hex[16] = "0123456789abcdef";
 	int		i;
 
 	i = 0;
 	while (i < size)
 	{
 		if (i % 16 == 0)
-			printf("%p: %2x", &dst[i], dst[i]);
-		else if (i % 16 == 15)
+			printf("%p: ", &dst[i]);
+		printf("%c", hex[((unsigned char)((char *)dst)[i]) / 16]);
+		printf("%c", hex[((unsigned char)((char *)dst)[i]) % 16]);
+		if (i % 2 == 1)
+			printf(" ");
+		if (i % 16 == 15)
 		{
-			printf("%02x ", dst[i]);
 			for (int j = 15; j >= 0; --j)
-				printf("%c", dst[i - j]);
+				printf("%c", (unsigned char)((char *)dst)[i - j]);
 			printf("\n");
 		}
-		else if (i % 2 == 1)
-			printf("%02x ", dst[i]);
-		else
-			printf("%02x", dst[i]);
 		++i;
 	}
 	if (i % 2 == 1)
 		printf("   ");
+	else
+		printf("     ");
 	for (int j = i % 16; j > 0; --j)
-	{
-		printf("%c", dst[i - j]);
-		if (j == 1)
-			printf("\n");
-	}
+		printf("%c", (unsigned char)((char *)dst)[i - j]);
+	printf("\n");
 }
 
 void	ft_show_argument_test(t_argument *arg)
@@ -70,6 +69,7 @@ int main(int argc, char **argv, char **env)
 	while (1)
 	{
 		str = readline("minishell-4.2$ ");
+		ft_print_memory(str, ft_strlen(str));
 		add_history(str);
 		arg = ft_parser(str, &environment);
 		if (arg == 0)

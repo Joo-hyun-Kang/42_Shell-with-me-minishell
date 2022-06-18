bfeadfa0c44c3a7c3cfdb7794cdb3202a151e3bc
jokang <autoba9687@gmail.com>
2022-06-18 22:52:22 +0900

    Refactor: pipe refactoring

--- src/execute_cmd/cmd.c
+++ src/execute_cmd/cmd.c
@@ -1,4 +1,3 @@
-#include "../../include/minishell.h"
 #include "cmd.h"
 
 /*
@@ -10,142 +9,256 @@ void	ft_print_error()
 	write(1, "Error!", 6);
 }
 
-int	ft_execute_single_cmd(t_argument *arg)
-{
-	enum e_builtin_type		bull_type;
-
-	if (is_builtin(arg->pa_argument[COMMAND_POSITION], &bull_type) == true)
-	{
-		ft_builtin(arg, bull_type, true);
-		return (1);
-	}
-	return (0);
-}
-
-void	ft_state_pipe(int fd_pipe1[2], int fd_pipe2[2], int fd_temp, int state)
+void	ft_set_pipe(t_pipes *pipes, int state)
 {
 	if (state == PIPE_START)
 	{
 		//FD1 : STDOUT -> PIPE OUT
 		// 파이프 실패 예외처리 해줄것
-		close(fd_pipe2[PIPE_WRITE]);
-		close(fd_pipe2[PIPE_READ]);
+		close(pipes->pipe2[PIPE_WRITE]);
+		close(pipes->pipe2[PIPE_READ]);
 
-		dup2(fd_pipe1[PIPE_WRITE], STDOUT_FILENO);
-		close(fd_pipe1[PIPE_WRITE]);
-		close(fd_pipe1[PIPE_READ]);
+		dup2(pipes->pipe1[PIPE_WRITE], STDOUT_FILENO);
+		close(pipes->pipe1[PIPE_WRITE]);
+		close(pipes->pipe1[PIPE_READ]);
 	}
 	else if (state == PIPE_MIDDLE)
 	{
-		close(fd_pipe1[PIPE_WRITE]);
+		close(pipes->pipe1[PIPE_WRITE]);
 
 		// 파이프 실패 예외처리 해줄것
-		dup2(fd_pipe2[PIPE_WRITE], STDOUT_FILENO);
-		close(fd_pipe2[PIPE_WRITE]);
-		close(fd_pipe2[PIPE_READ]);
+		dup2(pipes->pipe2[PIPE_WRITE], STDOUT_FILENO);
+		close(pipes->pipe2[PIPE_WRITE]);
+		close(pipes->pipe2[PIPE_READ]);
 		// 파이프 실패 예외처리 해줄것
-		dup2(fd_pipe1[PIPE_READ], STDIN_FILENO);
-		close(fd_pipe1[PIPE_READ]);
-		close(fd_pipe1[PIPE_WRITE]);
+		dup2(pipes->pipe1[PIPE_READ], STDIN_FILENO);
+		close(pipes->pipe1[PIPE_READ]);
+		close(pipes->pipe1[PIPE_WRITE]);
 	}
 	else if (state == PIPE_END)
 	{
-		close(fd_pipe1[PIPE_WRITE]);
-		close(fd_pipe2[PIPE_WRITE]);
+		close(pipes->pipe1[PIPE_WRITE]);
+		close(pipes->pipe2[PIPE_WRITE]);
 
 		// 파이프 실패 예외처리 해줄것
-		dup2(fd_temp, STDIN_FILENO);
-		close(fd_temp);
+		dup2(pipes->out_copy, STDIN_FILENO);
+		close(pipes->out_copy);
 
-		close(fd_pipe2[PIPE_READ]);
-		close(fd_pipe1[PIPE_READ]);
+		close(pipes->pipe2[PIPE_READ]);
+		close(pipes->pipe1[PIPE_READ]);
+	}
+}
+
+void	ft_fork_execute(t_argument *argument)
+{
+	pid_t	pid;
+
+	pid = fork();
+	if (pid == -1)
+		ft_print_error();
+	if (pid == 0)
+		ft_execute(argument, false);
+}
+
+void	ft_execute_single_cmd(t_argument **arg)
+{
+	enum e_builtin_type		bull_type;
+
+	if (is_builtin((*arg)->pa_argument[COMMAND_POSITION], &bull_type) == true)
+		ft_builtin((*arg), bull_type, true);
+	else
+		ft_fork_execute(*arg);
+	(*arg) = (*arg)->next;
+}
+
+void	ft_redir_lt(t_argument **arg)
+{
+	enum e_token_type t_type = (*arg)->next_token_type;
+
+	
+
+
+	(*arg) = (*arg)->next;
+}
+
+void	ft_redir_gt(t_argument **arg)
+{
+	enum e_token_type	t_type = (*arg)->next_token_type;
+	t_argument			*pre_arg;
+	int					fd;
+	int					fd_temp;
+
+	pre_arg = *arg;
+	(*arg) = (*arg)->next;
+
+	int length = ft_get_length_2d_arr((*arg)->pa_argument);
+	fd = open((*arg)->pa_argument[length - 1], O_RDONLY);
+	//open 함수 예외처리 나중에 더 추가
+	if (fd < 0)
+	{
+		printf("minshell: %s: %s\n", (*arg)->pa_argument[length - 1], strerror(errno));
+		return ;
+	}
+
+	fd_temp = dup(STDIN_FILENO);
+	dup2(fd, STDIN_FILENO);
+
+	ft_execute_single_cmd(&pre_arg);
+	close(fd);
+	dup2(fd_temp, STDIN_FILENO);
+}
+
+void	ft_redir(t_argument **arg)
+{
+	if ((*arg)->next_token_type == LT
+	|| (*arg)->next_token_type == DLT)
+		ft_redir_lt(arg);
+	else
+		ft_redir_gt(arg);
+	(*arg) = (*arg)->next;
+}
+
+int		ft_is_redir(enum e_token_type token)
+{
+	if (token == LT)
+		return (true);
+	else if (token == DLT)
+		return (true);
+	else if (token == GT)
+		return (true);
+	else if (token == DGT)
+		return (true);
+	return (false);
+}
+
+int	ft_construt_pipes(t_pipes *pipes)
+{
+	int ret1;
+	int ret2;
+
+	ret1 = pipe(pipes->pipe1);
+	ret2 = pipe(pipes->pipe2);
+	pipes->out_copy = pipes->pipe1[PIPE_READ];
+	pipes->out_copy_state = PIPE1_READ;
+
+	if (ret1 < -1 || ret2 < -1)
+	{
+		printf("minishell: %s\n", strerror(errno));
+		return (false);
+	}
+	
+	return (true);
+}
+
+void	ft_execute_mult_cmd(t_argument **arg)
+{
+	t_pipes				*pa_pipes;
+	enum e_token_type	token;
+	int	state;
+	int	ret;
+
+	
+	pa_pipes = (t_pipes *)malloc(sizeof(t_pipes));
+	ret = ft_construt_pipes(pa_pipes);
+	// 요거 실패했을 떄 처리 따로 할 것
+	if (ret == false)
+		return ;
+
+	state = INIT;
+	while (*arg != NULL)
+	{
+		token = (*arg)->next_token_type;
+		
+		if (token == PIPE)
+		{
+			int temp_pipe;
+			ft_execute_pipe(arg, state, pa_pipes);
+		}
+		else if (ft_is_redir(token))
+		{
+			state = REDIRECTION;
+		}
+	}
+
+	close(pa_pipes->pipe1[PIPE_WRITE]);
+	close(pa_pipes->pipe1[PIPE_READ]);
+	close(pa_pipes->pipe2[PIPE_WRITE]);
+	close(pa_pipes->pipe2[PIPE_READ]);
+
+	while (wait(NULL) != -1)
+	{
+	}
+	
+	//free(pipes)
+}
+
+void	ft_execute_pipe(t_argument **arg, int state, t_pipes *pipes)
+{
+	if (state == INIT)
+	{
+		state = PIPE_START;
+		pipes->out_copy = pipes->pipe1[PIPE_READ];
+		pipes->out_copy_state = PIPE1_READ;
+	}
+	else if (state == PIPE_START)
+	{
+		state = PIPE_MIDDLE;
+		if (pipes->out_copy_state == PIPE1_READ)
+		{
+			pipes->out_copy = pipes->pipe2[PIPE_READ];
+			pipes->out_copy_state = PIPE2_READ;
+		}
+		else
+		{
+			pipes->out_copy = pipes->pipe1[PIPE_READ];
+			pipes->out_copy_state = PIPE1_READ;
+		}
+	}
+	else if (state == END)
+		state = PIPE_END;
+
+	pid_t pid;
+	pid = fork();
+	if (pid == -1)
+	{
+		printf("minishell : %s\n", strerror(errno));
+		//return?
+	}
+	else if (pid == 0)
+	{
+		ft_set_pipe(pipes, state);
+		ft_execute(*arg, false);
+	}
+
+	*arg = (*arg)->next;
+	if (*arg == NULL)
+		return ;
+
+	enum e_token_type token = (*arg)->next_token_type;
+	if (token == PIPE)
+	{
+		ft_execute_pipe(arg, state, pipes);
+	}
+	else if (token == EOL || token == SEMICOLON)
+	{
+		ft_execute_pipe(arg, END, pipes);
+	}
+	else if (ft_is_redir(token))
+	{
+		//ft_execute_redir(arg, state, pipes);
 	}
 }
 
 void	ft_system(t_argument *argument)
 {
-	enum e_token_type		t_type;
-	int						fd_pipe1[PIPE_COUNT];
-	int						fd_pipe2[PIPE_COUNT];
-	int						fd_temp;
-	int						state;
-	pid_t					child_pid;
-
-	state = INIT;
 	while (argument != NULL)
 	{
-		t_type = argument->next_token_type;
-
-		// INIT 상태 일 때, 전이 조건
-		if (state == INIT)
-		{
-			if (t_type == EOL || t_type == SEMICOLON)
-				state = SINGLE_CMD;
-			else if (t_type == PIPE)
-				state = PIPE_START;
-			else
-				state = SINGLE_REDIRECTION;
-		}
-		// INIT 상태가 아닐 때, 전이 조건
-		else if (t_type == EOL || t_type == SEMICOLON)
-			state = PIPE_END;
-		else if (t_type == PIPE)
-			state = PIPE_MIDDLE;
+		// 첫번쨰 친구들은 EOL 이거나 세미콜론은 여기서 막힌다
+		if (argument->next_token_type == EOL || argument->next_token_type == SEMICOLON)
+			ft_execute_single_cmd(&argument);
 		else
-			state = MULTI_REDIRECTION;
-
-		// 임시 분리
-		if (state == PIPE_START)
-		{
-			pipe(fd_pipe1);
-			pipe(fd_pipe2);
-			fd_temp = fd_pipe1[PIPE_READ];
-		}
-		else if (state == PIPE_MIDDLE)
-			fd_temp = fd_pipe2[PIPE_READ];
-
-		// SINGLE STATE
-		if (state == SINGLE_REDIRECTION || state == MULTI_REDIRECTION)
-		{
-			// 파이프() 연결
-			printf("리다이렉션 추가 예정\n");
-			// 리다이렉션 인자
-			argument = argument->next;
-			// 다음 argument
-			argument = argument->next;
-			state = INIT;
-			continue ;
-		}
-		if (state == SINGLE_CMD)
-		{
-			if (ft_execute_single_cmd(argument) == true)
-			{
-				argument = argument->next;
-				state = INIT;
-				continue ;
-			}
-			// BUILTIN 함수가 아니라면 fork()
-		}
-
-		child_pid = fork();
-		if (child_pid == -1)
-			ft_print_error();
-		if (child_pid == 0)
-		{
-			ft_state_pipe(fd_pipe1, fd_pipe2, fd_temp, state);
-			ft_execute(argument, false);
-		}
-		argument = argument->next;
+			ft_execute_mult_cmd(&argument);
 	}
-
-	if (state == PIPE_END)
-	{
-		close(fd_pipe1[PIPE_READ]);
-		close(fd_pipe1[PIPE_WRITE]);
-		close(fd_pipe2[PIPE_READ]);
-		close(fd_pipe2[PIPE_WRITE]);
-	}
-
 	while (wait(NULL) != -1);
 }
 
@@ -216,26 +329,28 @@ void	ft_execute(t_argument *argument, int is_parent)
 	command = argument->pa_argument[COMMAND_POSITION];
 	bull_type = INVAILD;
 
-	if (is_builtin(command, &bull_type))
+	if (is_builtin(command, &bull_type) == true)
 	{
 		ft_builtin(argument, bull_type, is_parent);
 		return ;
 	}
 
 	//CASE1 : 절대 경로나 상대경로로 들어오는 경우
+	is_path = false;
 	if (ft_strchr(argument->pa_argument[COMMAND_POSITION], '/') != false)
 		is_path = true;
 	else
 		is_path = false;
 
 	if (is_path)
+	{
 		ft_execute_path(argument);
+	}
 
 	//CASE2 : 경로 없이 COMMAND만 들어왔는데 환경변수를 훑었을 때 명령어가 있는 경우
-	pa_path = ft_search_command_path_malloc(argument->pa_argument[COMMAND_POSITION]);
+	pa_path = ft_search_command_path_malloc(argument->env, argument->pa_argument[COMMAND_POSITION]);
 	if (pa_path != NULL)
 		ft_execute_nopath(argument, pa_path);
-
 	//CASE3 : 커맨드이고 현재 디렉토리가 커맨드가 있는 곳(일반적으로 환경변수에 등록되는 곳)인 경우
 	//	      커맨드와 현재 디렉토리 내에 있으면 그냥 실행함
 	if (ft_is_command_dir())
@@ -276,9 +391,8 @@ int	ft_is_command_dir()
 	return (false);
 }
 
-char	*ft_search_command_path_malloc(char *command)
+char	*ft_search_command_path_malloc(t_env_root *root, char *command)
 {
-	char	**pp;
 	char	*env_path;
 	char	**pa_directories;
 	int		length;
@@ -286,18 +400,13 @@ char	*ft_search_command_path_malloc(char *command)
 	int		position;
 	char	*pa_temp;
 	char	*pa_command_with_path;
+	t_env	*env_path_node;
 
-	env_path = NULL;
-	pp = environ;
-	while (*pp != NULL)
-	{
-		if (ft_strncmp(*pp, "PATH", 4) == 0)
-		{
-			env_path = *pp;
-			break;
-		}
-		pp++;
-	}
+	env_path_node = ft_env_search(root, ft_strdup("PATH"));
+	if (env_path_node == NULL)
+		env_path = NULL;
+	else
+		env_path = env_path_node->pa_value;
 
 	if (env_path == NULL)
 	{
@@ -305,20 +414,7 @@ char	*ft_search_command_path_malloc(char *command)
 		return (NULL);
 	}
 
-	if (env_path != NULL)
-	{
-		int env_path_length = ft_strlen(env_path);
-		if (env_path_length > ENV_PATH_NAME_LENGTH)
-		{
-			env_path += ENV_PATH_NAME_LENGTH;
-		}
-		else
-		{
-			//환경 변수 'PATH='인 상태 
-			return (NULL);
-		}
-		pa_directories = ft_split(env_path, ':');
-	}
+	pa_directories = ft_split(env_path, ':');
 
 	//현재 디렉토리도 넣는 로직 추가
 	i = 0;

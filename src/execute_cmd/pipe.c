c513815595ea19dae3886a354c6acbc98da200ae
jokang <autoba9687@gmail.com>
2022-06-23 16:11:11 +0900

    Comment: remove commnet and debug print

--- src/execute_cmd/pipe.c
+++ src/execute_cmd/pipe.c
@@ -12,9 +12,6 @@ void	ft_execute_pipe(t_argument **arg, int state, t_pipes *pipes)
 	pid_t	pid;
 	int		status;
 
-	// if (pipes->current_idx % 2 == 0)
-	// 	pipe(pipes->array[pipes->current_idx]);		
-
 	pid = fork();
 	if (pid == -1)
 	{
@@ -24,30 +21,15 @@ void	ft_execute_pipe(t_argument **arg, int state, t_pipes *pipes)
 	else if (pid == 0)
 	{
 		t_argument	*p = *arg;
-		while (p != NULL)
-		{
-			printf("%ld : %s\n", pipes->current_idx, p->pa_argument[0]);
-			p = p->next;
-		}
-		
 		ft_set_pipe(pipes, state);
 		ft_execute(*arg, false);
 	}
-	// ls | sort | sort | sort
-	//    0
-
-	// if (pipes->current_idx > 1 && pipes->current_idx % 2 == 1)
-	// {
-		
-	// }
 
 	pipes->current_idx++;
 	*arg = (*arg)->next;
 	if (*arg == NULL)
 		return ;
 
-	// 만약에 다음 토큰이 파이프라면 파이프 ㄱㄱ
-	// 재귀로 안 짜고 while문으로 빼려면 여기서 빼면 됌
 	enum e_token_type token = (*arg)->next_token_type;
 	if (token == PIPE)
 	{
@@ -77,17 +59,16 @@ int	ft_construt_pipes(t_argument *arg, t_pipes *pipes)
 	p = arg;
 	pipe_count = 0;
 	ret = 0;
-	
+
 	while (p != NULL)
 	{
 		if (p->next_token_type == PIPE)
 		{
 			pipe_count++;
 		}
-		printf("%s : %s\n", "init", p->pa_argument[0]);
 		p = p->next;
 	}
-
+	
 	pipes->array = (int **)malloc(sizeof(int *) * pipe_count);
 	pipes->pipe_count = pipe_count;
 	pipes->current_idx = 0;
@@ -112,32 +93,18 @@ void	ft_set_pipe(t_pipes *pipes, int state)
 	cur_pipe = pipes->array[pipes->current_idx];
 	if (state == PIPE_START)
 	{
-		//FD1 : STDOUT -> PIPE OUT
-		// 파이프 실패 예외처리 해줄것
 		dup2(cur_pipe[PIPE_WRITE], STDOUT_FILENO);
-		//close(cur_pipe[PIPE_WRITE]);
-		//close(cur_pipe[PIPE_READ]);
 	}
 	else if (state == PIPE_MIDDLE)
 	{
 		pre_pipe = pipes->array[pipes->current_idx - 1];
-		// 파이프 실패 예외처리 해줄것
 		dup2(pre_pipe[PIPE_READ], STDIN_FILENO);
-		//close(pre_pipe[PIPE_READ]);
-		//close(pre_pipe[PIPE_WRITE]);
-		
-		// 파이프 실패 예외처리 해줄것
 		dup2(cur_pipe[PIPE_WRITE], STDOUT_FILENO);
-		//close(cur_pipe[PIPE_WRITE]);
-		//close(cur_pipe[PIPE_READ]);
 	}
 	else if (state == PIPE_END)
 	{
 		pre_pipe = pipes->array[pipes->current_idx - 1];
-		// 파이프 실패 예외처리 해줄것
 		dup2(pre_pipe[PIPE_READ], STDIN_FILENO);
-		//close(pre_pipe[PIPE_READ]);
-		//close(pre_pipe[PIPE_WRITE]);
 	}
 
 	i = 0;
@@ -145,8 +112,6 @@ void	ft_set_pipe(t_pipes *pipes, int state)
 	{
 		close(pipes->array[i][PIPE_READ]);
 		close(pipes->array[i][PIPE_WRITE]);
-		ft_putnbr_fd(pipes->current_idx, 2);
-
 		i++;
 	}
 }

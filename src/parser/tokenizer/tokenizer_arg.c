a3d35781f3a3f1fae46eb749916f784a8086ef4d
kanghyki <kanghyki@gmail.com>
2022-06-24 07:10:20 +0900

    Fix: 개행 문자와 `~'가 입력되는 경우 `~'이 파싱되지 않는 버그 수정

--- src/parser/tokenizer/tokenizer_arg.c
+++ src/parser/tokenizer/tokenizer_arg.c
@@ -6,7 +6,7 @@
 /*   By: kanghyki <kanghyki@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/16 15:51:20 by kanghyki          #+#    #+#             */
-/*   Updated: 2022/06/23 21:23:04 by kanghyki         ###   ########.fr       */
+/*   Updated: 2022/06/24 07:09:26 by kanghyki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,7 +47,7 @@ void	tk_replace_home(t_lexer *lexer)
 	t_env	*node;
 
 	lx_read(lexer);
-	if (lx_chr(lexer) != '\0' && ft_strchr(M_SPACE, lx_chr(lexer)) != NULL)
+	if (ft_strchr(M_SPACE, lx_chr(lexer)) != NULL)
 	{
 		node = ft_env_search(lx_env(lexer), ft_strdup("HOME"));
 		if (node != NULL)

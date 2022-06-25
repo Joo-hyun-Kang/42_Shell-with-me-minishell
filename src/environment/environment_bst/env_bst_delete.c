febae4fbcc45a6840b545bc49f737b4163095eb0
kanghyki <kanghyki@gmail.com>
2022-06-25 15:50:14 +0900

    Fix: env_bst_delete 수정

--- src/environment/environment_bst/env_bst_delete.c
+++ src/environment/environment_bst/env_bst_delete.c
@@ -6,7 +6,7 @@
 /*   By: kanghyki <kanghyki@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/17 09:14:46 by kanghyki          #+#    #+#             */
-/*   Updated: 2022/06/24 19:27:08 by kanghyki         ###   ########.fr       */
+/*   Updated: 2022/06/25 15:50:07 by kanghyki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,12 @@
 static void	ft_env_delete_if_both_null(t_env_root *root, t_env *cur_node);
 static void	ft_env_delete_if_left_null(t_env_root *root, t_env *cur_node);
 static void	ft_env_delete_if_right_null(t_env_root *root, t_env *cur_node);
-static void	ft_env_delete_if_both_not_null(t_env_root *root, t_env *cur_node);
+static void	ft_env_delete_if_not_null(t_env_root *root, t_env *cur, t_env *rep);
 
 void	ft_env_delete(t_env_root *root, char *key)
 {
 	t_env	*cur_node;
+	t_env	*replace_node;
 
 	cur_node = ft_env_search(root, key);
 	if (cur_node == NULL)
@@ -31,7 +32,12 @@ void	ft_env_delete(t_env_root *root, char *key)
 	else if (cur_node->right == NULL)
 		ft_env_delete_if_right_null(root, cur_node);
 	else
-		ft_env_delete_if_both_not_null(root, cur_node);
+	{
+		replace_node = cur_node->right;
+		while (replace_node->left)
+			replace_node = replace_node->left;
+		ft_env_delete_if_not_null(root, cur_node, replace_node);
+	}
 	--(root->size);
 	ft_free_env_node(cur_node);
 }
@@ -77,30 +83,27 @@ static void	ft_env_delete_if_right_null(t_env_root *root, t_env *cur_node)
 	}
 }
 
-static void	ft_env_delete_if_both_not_null(t_env_root *root, t_env *cur_node)
+static void	ft_env_delete_if_not_null(t_env_root *root, t_env *cur, t_env *rep)
 {
-	t_env	*replace_node;
-
-	replace_node = cur_node->right;
-	while (replace_node->left)
-		replace_node = replace_node->left;
-	if (cur_node->parent == NULL)
-		root->root = replace_node;
+	if (rep->parent->left == rep)
+		rep->parent->left = NULL;
+	else
+		rep->parent->right = NULL;
+	if (cur->parent == NULL)
+		root->root = rep;
 	else
 	{
-		if (cur_node->parent->left == cur_node)
-			cur_node->parent->left = replace_node;
+		if (cur->parent->left == cur)
+			cur->parent->left = rep;
 		else
-			cur_node->parent->right = replace_node;
-		replace_node->parent = cur_node->parent;
+			cur->parent->right = rep;
 	}
-	if (cur_node->right == replace_node)
-		replace_node->left = cur_node->left;
+	rep->parent = cur->parent;
+	if (cur->right == rep)
+		rep->left = cur->left;
 	else
 	{
-		replace_node->left = cur_node->left;
-		replace_node->right = cur_node->right;
+		rep->left = cur->left;
+		rep->right = cur->right;
 	}
-	replace_node->left->parent = replace_node;
-	replace_node->right->parent = replace_node;
 }
